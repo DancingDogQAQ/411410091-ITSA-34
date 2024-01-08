9b77048aa74dc0ca7c9a4325f41231cb18451130
DancingDogQAQ <131145530+DancingDogQAQ@users.noreply.github.com>
2024-01-09 03:19:59 +0800

    Add files via upload

---
+++ ar47.c
@@ -0,0 +1,23 @@
+//https://e-tutor.itsa.org.tw/e-Tutor/mod/programming/view.php?a=15535
+
+#include<stdio.h>  
+#include<stdlib.h>  
+  
+int main(){  
+    int input;  
+    while(scanf("%d",&input)!=EOF){  
+        while(input>=5 && input%5==0){  
+            input/=5;  
+        }  
+        while(input>=3 && input%3==0){  
+            input/=3;  
+        }  
+        while(input>=2 && input%2==0){  
+            input/=2;  
+        }  
+        if(input==1) puts("Regular number!");  
+        else puts("Irregular number!");  
+    }  
+    return 0;  
+}  
+////////
